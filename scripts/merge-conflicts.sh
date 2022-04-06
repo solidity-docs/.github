92c4c640e9dd8f1a0529566fb93f3596a3bbda3b
o-lenczyk <piahoo@gmail.com>
2022-04-06 14:46:25 +0200

    Translation bot: do not create PR if sync branch exists

--- scripts/merge-conflicts.sh
+++ scripts/merge-conflicts.sh
@@ -17,6 +17,18 @@ sync_branch="sync-$(git describe --tags --always english/develop)"
 # pass the hash and the branch name to action "create PR"
 echo "::set-output name=branch_name::$sync_branch"
 
+# check if sync branch exists
+if git ls-remote --exit-code --heads origin "$sync_branch"
+then
+    branch_exists=true
+    echo "sync_branch $sync_branch exists"
+else
+    branch_exists=false
+    echo "sync_branch $sync_branch does not exist"
+fi
+
+echo "::set-output name=branch_exists::$branch_exists"
+
 # pull from ethereum/solidity develop
 git pull english develop --rebase=false --squash || true
 
